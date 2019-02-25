f1e8ea77d5c9198a6b8844fac8052a07d18c7df7
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-02-25 13:18:03 -0800

    add initial styles to title wrapper, icons wrapper and icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,20 +8,54 @@ import finishIcon from '../assets/images/finish-icon.png';
 import deleteIcon from '../assets/images/delete-icon.png';
 
 function Header(props) {
+
+  const headerTitleWrapperStyles = {
+    height: '68px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000',
+    fontSize: '26px',
+    color: '#FFF'
+  }
+  const headerIconsWrapperStyles = {
+    height: '47px',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#DBDBDB'
+  }
+  const homeIconStyles = {
+    width: '29px',
+    height: '29px'
+  }
+  const exerciseArchiveIconStyles = {
+    width: '30px',
+    height: '26px'
+  }
+  const finishIconStyles = {
+    width: '25px',
+    height: '25px'
+  }
+  const deleteIconStyles = {
+    width: '30px',
+    height: '30px'
+  }
+
   return(
     <div>
-      <div>
+      <div style={headerTitleWrapperStyles}>
         <h1>W-O-T</h1>
       </div>
-      <div>
+      <div style={headerIconsWrapperStyles}>
         <Link to='/'>
-          <img src={homeIcon} alt='Home Icon'/>
+          <img src={homeIcon} alt='Home Icon' style={homeIconStyles}/>
         </Link>
         <Link to='/exerciseArchive'>
-          <img src={archiveIcon} alt='Archive Icon'/>
+          <img src={archiveIcon} alt='Archive Icon' style={exerciseArchiveIconStyles}/>
         </Link>
-        <img src={finishIcon} alt='Finish Icon'/>
-        <img src={deleteIcon} alt='Delete Icon'/>
+        <img src={finishIcon} alt='Finish Icon' style={finishIconStyles}/>
+        <img src={deleteIcon} alt='Delete Icon' style={deleteIconStyles}/>
       </div>
     </div>
   );
