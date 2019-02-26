1b1bbdf015378a14698d0c30b5c5cebd201b0f02
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-02-25 17:43:46 -0800

    add object literal to SetList parameter containing setList, exerciseID and onInputChange props which are defined, pass setID, setNumber, weight, reps, exerciseID and onInputChange props to Set component

--- src/components/SetList.js
+++ src/components/SetList.js
@@ -2,12 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Set from './Set';
 
-function SetList(props) {
+function SetList({ setList, exerciseID, onInputChange }) {
   return(
     <div>
-      <Set />
+      {
+        setList.forEach((set) => {
+          return(
+            <Set
+              key={set.setID}
+              setID={set.setID}
+              setNumber={set.setNumber}
+              weight={set.weight}
+              reps={set.reps}
+              exerciseID={exerciseID}
+              onInputChange={onInputChange}
+            />
+          );
+        })
+      }
     </div>
   );
+}
+
+SetList.propTypes = {
+  setList: PropTypes.array,
+  exerciseID: PropTypes.number,
+  onInputChange: PropTypes.func
 }
 
 export default SetList;
