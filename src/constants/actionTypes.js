705f5a5f500b38d1a37d7714be8dab62cb563617
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-03-01 23:09:37 -0800

    add DELETE_CHECKED actionType

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,3 +1,4 @@
 export const ADD_EXERCISE = 'ADD_EXERCISE';
 export const ADD_SET = 'ADD_SET';
 export const ADD_CHECKED_CHECKBOX_ID = 'ADD_CHECKED_CHECKBOX_ID';
+export const DELETE_CHECKED = 'DELETE_CHECKED';
