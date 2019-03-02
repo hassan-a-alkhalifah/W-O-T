90d8b4fafcfc29c807fb131d8b738b32f5fe236b
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-03-01 19:52:12 -0800

    add testcase to check if checkbox if checked that the id of check input is stored in appropriate array, add tsstcase to check if checkbox is  unchecked and that the id of the unchecked input is removed from appropriate array

--- src/__tests__/reducers/checkbox-checked-list-reducer.test.js
+++ src/__tests__/reducers/checkbox-checked-list-reducer.test.js
@@ -7,11 +7,44 @@ const initialState = {
   exerciseCheckedList: [],
   setCheckedList: []
 }
+let action;
 
 describe('checkboxCheckedListReducer', () => {
 
   test('Should return default state if no action type is recognized', () => {
     expect(checkboxCheckedListReducer(initialState, { type: null })).toEqual(initialState);
+  });
+
+  test('Should add ID of input type if checked to input type array list', () => {
+    action = {
+      type: c.ADD_CHECKED_CHECKBOX_ID,
+      ifChecked: true,
+      whichInputTypeChecked: 'exerciseCheckedList',
+      inputID: '1234'
+    }
+    expect(checkboxCheckedListReducer(initialState, action)).toEqual(
+      {
+        workoutCheckedList: [],
+        exerciseCheckedList: ['1234'],
+        setCheckedList: []
+      }
+    );
+  });
+
+  test('Should remove ID of input type if unchecked from input type array list', () => {
+    action = {
+      type: c.ADD_CHECKED_CHECKBOX_ID,
+      ifChecked: false,
+      whichInputTypeChecked: 'exerciseCheckedList',
+      inputID: '1234'
+    }
+    expect(checkboxCheckedListReducer(initialState, action)).toEqual(
+      {
+        workoutCheckedList: [],
+        exerciseCheckedList: [],
+        setCheckedList: []
+      }
+    );
   });
 
 });
