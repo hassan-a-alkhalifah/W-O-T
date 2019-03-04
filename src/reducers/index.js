3f722fd4cbc0b52a95de19f96536d81d2e35c629
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-03-03 17:02:19 -0800

    import workoutNotesReducer and add to combineReducers argument object

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import workoutListReducer from './workout-list-reducer';
 import newWorkoutExerciseListReducer from './new-workout-exercise-list-reducer';
 import checkboxCheckedListReducer from './checkbox-checked-list-reducer';
+import workoutNotesReducer from './workout-notes-reducer';
 
 const rootReducer = combineReducers({
   masterWorkoutList: workoutListReducer,
   newWorkoutMasterExerciseList: newWorkoutExerciseListReducer,
-  checkboxCheckedLists: checkboxCheckedListReducer
+  checkboxCheckedLists: checkboxCheckedListReducer,
+  workoutNotes: workoutNotesReducer
 });
 
 export default rootReducer;
