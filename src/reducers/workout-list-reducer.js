00063a7e675ae1d5875f09dff18d72a2ee6ac68b
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-03-11 16:42:34 -0700

    import contacts, interpolate newWorkout from action object, add switch case which contains RECEIVE_WORKOUT case and default

--- src/reducers/workout-list-reducer.js
+++ src/reducers/workout-list-reducer.js
@@ -1,6 +1,16 @@
-// import constants from './../constants';
-// const { c } = constants;
+import constants from './../constants';
+const { c } = constants;
 
-export default (state = {}, action) => {
+export default (state = [], action) => {
+
+  const { newWorkout } = action;
+
+  switch(action.type) {
+    case c.RECEIVE_WORKOUT: {
+      return [...state, newWorkout];
+    }
+    default:
     return state;
+
+  }
 };
