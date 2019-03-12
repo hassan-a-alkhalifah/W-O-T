bbbcd29039f061c372e1a29d053726fa5640d924
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-03-12 16:24:46 -0700

    add pageType key value to initialState object, add CHANGE_PAGE_TYPE case

--- src/reducers/page-check-reducer.js
+++ src/reducers/page-check-reducer.js
@@ -3,7 +3,8 @@ import constants from './../constants';
 const { c } = constants;
 const initialState = {
   homePage: true,
-  archivePage: false
+  archivePage: false,
+  pageType: '/'
 };
 
 export default (state = initialState, action) => {
@@ -12,11 +13,14 @@ export default (state = initialState, action) => {
 
   switch(action.type) {
     case c.CHANGE_PAGE_STATE: {
-      if(pageType === 'archive') {
+      if(state.pageType === '/exerciseArchive') {
         return {...state, homePage: false, archivePage: true};
       } else {
         return {...state, homePage: true, archivePage: false};
       }
+    }
+    case c.CHANGE_PAGE_TYPE: {
+      return {...state, pageType: pageType};
     }
     default: {
       return state;
